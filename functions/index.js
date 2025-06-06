71249b5a3801912a81e8944434d36ca29be322bc
selami538 <erdaltayfun37@gmail.com>
2025-06-06 16:57:57 +0300

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -312,7 +312,7 @@ display: block;
 <div data-rc='[{"icon":"home","text":"Anasayfa","link":"/","target":"_self"},{"icon":"fas fa-newspaper","text":"Spor Bahisleri","link":/","target":"_self"},{"icon":"fas fa-futbol","text":"Canl\u0131 Bahis","link":"","target":"_self"},{"icon":"fas fa-play","text":"Casino","link":"","target":"_self"},{"icon":"fas fa-tv","text":"Canl\u0131 Casino","link":"","target":"_self"},{"icon":"fas fa-basketball-ball","text":"Promosyonlar","link":"","target":"_self"},{"icon":"fas fa-fas fa-award","text":" Tombala","link":"","target":"_self"}]'>
 <script src="assets/js/rc.js"></script>
 </div>
-<center><img class="" src="logo.png" width="300"alt="Canlı maç yayınları" /></center>
+<center><img class="" src="${logo}" width="300"alt="Canlı maç yayınları" /></center>
 <div class="copyright-text">
         <p>Bu sitenin altyapısı <a href="https://zirvestream.com/"><b>Zirve Stream</b></a> tarafından sağlanmıştır.</p>
     </div>
