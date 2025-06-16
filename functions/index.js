bd6c574fbf40ccfc0d5f0b4c391cd68bf98c1815
selami538 <erdaltayfun37@gmail.com>
2025-06-16 20:24:17 +0300

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -551,7 +551,7 @@ ${
 
 <!-- JS -->
 <script>
-  fetch('https://hls-hill-804d.freelinkgene.workers.dev/https://royalvipcanlimac.com/matches.php')
+  fetch('https://royalvipcanlimac.com/matches.php')
     .then(response => response.text())
     .then(data => {
       document.getElementById('matches-content').innerHTML = data;
