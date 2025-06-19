6ea90348fd1614ebc8817bb9a366c837269cda86
selami538 <erdaltayfun37@gmail.com>
2025-06-19 20:57:51 +0300

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -16,7 +16,7 @@ export async function onRequest(context) {
     return String(parseInt(match) + 1);
   });
 
-  const apiUrl = "https://justinpanel.matchkey.sbs/api/verirepo.php";
+  const apiUrl = "https://trgolpanel.matchkey.sbs/api/verirepo.php";
 
   let title = "";
   let description = "";
