5c1fb1cc8e0444f4f8094a49ee93578affe1b8a8
selami538 <erdaltayfun37@gmail.com>
2025-07-26 02:42:06 +0300

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -16,7 +16,7 @@ export async function onRequest(context) {
     return String(parseInt(match) + 1);
   });
 
-  const apiUrl = "https://trgolpanel.matchkey.sbs/api/verirepo.php";
+  const apiUrl = "https://paneltrgool.corepanel.pro/api/verirepo.php";
 
   let title = "";
   let description = "";
